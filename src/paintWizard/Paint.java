958734bac63cd294750709591d1e623b8178ad19
Luke Montgomery <luke.p.montgomery@gmail.com>
2018-12-13 08:58:23 +0000

    Fixed costOfCoverage method

--- src/paintWizard/Paint.java
+++ src/paintWizard/Paint.java
@@ -1,55 +1,49 @@
 package paintWizard;
 
 public class Paint {
-	
+
 	private double costPerTin = 0;
 	private double coveragePerTin = 0;
 	private double sizeOfRoom = 0;
 	private String name = "";
-	
-	public Paint(String name,int cost, int coverage) {
+
+	public Paint(String name, int cost, int coverage) {
 		this.costPerTin = cost;
 		this.coveragePerTin = coverage;
 		this.name = name;
 	}
-	
 
 	public double getCostPerM2(Paint paint) {
 		return paint.costPerTin / paint.coveragePerTin;
 	}
 
 	public String compareCost(Paint paint, Paint paint2) {
-		
+
 		if (paint.getCostPerM2(paint) < paint2.getCostPerM2(paint2)) {
 			return paint.getName() + " is cheaper";
 		}
 		if (paint.getCostPerM2(paint) > paint2.getCostPerM2(paint2)) {
 			return paint2.getName() + " is cheaper";
-		}
-		else {
+		} else {
 			return "Both are the same price";
 		}
 	}
-	
+
 	public double calculateWaste(double sizeOfRoom, Paint coverage) {
-		
-		while (sizeOfRoom > coverage.coveragePerTin ) {
-			
-			coverage.coveragePerTin = coverage.coveragePerTin + coverage.coveragePerTin;
-			coverage.costPerTin = coverage.costPerTin + coverage.costPerTin;
-			
-			
+		double costPerTinTemp = coverage.costPerTin;
+		double coveragePerTinTemp = coverage.coveragePerTin;
+		while (sizeOfRoom > coverage.coveragePerTin) {
+
+			coverage.coveragePerTin = coverage.coveragePerTin + coveragePerTinTemp;
+			coverage.costPerTin = coverage.costPerTin + costPerTinTemp;
+
 		}
-		
-		double waste = coverage.coveragePerTin - sizeOfRoom;
-		
-		return waste;
+
+		return coverage.coveragePerTin - sizeOfRoom;
 	}
-	
-	
+
 	public String getName() {
 		return this.name;
 	}
 
-			
 }
